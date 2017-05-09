46a7bf768a39c760b8e5815f1e2b9a07a48f1006
smallnest <smallnest@gmail.com>
2017-05-09 15:31:45 +0800

    add delimiterBasedFrameConn

---
+++ delimiter_based_frameconn.go
@@ -0,0 +1,52 @@
+package goframe
+
+import (
+	"bufio"
+	"net"
+)
+
+type delimiterBasedFrameConn struct {
+	delimiter byte
+	c         net.Conn
+	r         *bufio.Reader
+	w         *bufio.Writer
+}
+
+func (fc *delimiterBasedFrameConn) ReadFrame() ([]byte, error) {
+	var (
+		isPrefix bool
+		err      error
+		line, ln []byte
+	)
+
+	for isPrefix && err == nil {
+		line, err = fc.r.ReadBytes(fc.delimiter)
+		ln = append(ln, line...)
+		if err != nil {
+			return ln, err
+		}
+	}
+
+	return ln, nil
+}
+
+func (fc *delimiterBasedFrameConn) WriteFrame(p []byte) error {
+	_, err := fc.w.Write(p)
+	if err != nil {
+		return err
+	}
+	err = fc.w.WriteByte(fc.delimiter)
+	if err != nil {
+		return err
+	}
+	fc.w.Flush()
+	return nil
+}
+
+func (fc *delimiterBasedFrameConn) Close() error {
+	return fc.c.Close()
+}
+
+func (fc *delimiterBasedFrameConn) Conn() net.Conn {
+	return fc.c
+}
